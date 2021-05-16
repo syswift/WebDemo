540e521802a46fb0a019d636481e4c52069f73fe
syswift <myworlddai97@gmail.com>
2021-05-16 17:31:01 +1200

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 app.use(json());
 const port = process.env.PORT || 5000;
 
+app.get('/api/users/currentuser', (req, res) =>{
+    console.log('Hi there!');
+})
+
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}!`);
 })
