794cbebf457cc929a84445f7f2db9a0807ffffc8
syswift <myworlddai97@gmail.com>
2021-05-16 21:04:08 +1200

    auth-currentUser

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {currentUserRouter} from './routes/currentUser';
 
 const app = express();
 app.use(json());
-const port = process.env.PORT || 5000;
+app.use(currentUserRouter);
 
-app.get('/api/users/currentuser', (req, res) =>{
-    console.log('Hi there!');
-})
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}!`);
