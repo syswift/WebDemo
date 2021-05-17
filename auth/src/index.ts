82ecc79a04793161cb76efb08ce0c3583dcbdb8f
syswift <myworlddai97@gmail.com>
2021-05-17 12:45:58 +1200

    user-model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
+
 import {currentUserRouter} from './routes/currentUser';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -22,8 +24,23 @@ app.all('*', async (req, res) => {  //request not found
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDb');
+    } catch (err){
+        console.error(err);
+    }
 
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}!`);
-})
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, ()=>{
+        console.log(`Listening on port ${port}!`);
+    });
+};
+
+start();
