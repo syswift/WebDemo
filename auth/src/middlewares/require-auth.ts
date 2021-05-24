9f8a5ffcccb5d44e82a363cb509be6ea54d2a6c4
syswift <myworlddai97@gmail.com>
2021-05-24 17:27:50 +1200

    create

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,2 +1,7 @@
 import {Request, Response, NextFunction} from 'express';
 
+export const requireAuth = (req: Request, res: Response, next: NextFunction) =>{
+    if(!req.currentUser) {
+        return res.send(401).send();
+    }
+};
