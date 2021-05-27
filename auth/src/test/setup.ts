b9b096c88945f164ba06c8f9753d3461d57f3116
syswift <myworlddai97@gmail.com>
2021-05-27 23:51:11 +1200

    test-session-done

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import {app} from '../app';
+import request from 'supertest';
+
+declare global{
+    namespace NodeJS{
+        interface Global{
+            signup(): Promise<string[]>;
+        }
+    }
+}
 
 let mongo: any;
 
@@ -28,3 +37,20 @@ afterAll( async () => {
     await mongo.stop();
     await mongoose.connection.close();
 })
+
+global.signup = async () =>{
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/auth/signup')
+        .send({
+            email,password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+
+};
