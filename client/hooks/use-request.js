86241e801372589bef16ae9339d8098bed7488bb
syswift <myworlddai97@gmail.com>
2021-06-02 07:54:20 +1200

    security update

---
+++ client/hooks/use-request.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default ({ url, method, body}) => {
+    const [errors, setErrors] = useState(null);
+
+    const doRequest = () => {
+    };
+};
