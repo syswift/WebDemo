88d49e00a109339c9d2c13cbbccec49c2a55782f
syswift <myworlddai97@gmail.com>
2021-06-04 20:48:53 +1200

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,14 +5,17 @@ const start = async () => {
     if(!process.env.JWT_KEY) {
         throw new Error('JWT_KEY not found!');
     }
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO_URI must be defined');
+    }
 
     try{
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-        console.log('Connected to MongoDb');
+        console.log('Connected to MongoDb '+process.env.MONGO_URI);
     } catch (err){
         console.error(err);
     }
