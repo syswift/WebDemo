9bc23dfecc970b19e84efad206e11cb0dfcdb468
syswift <myworlddai97@gmail.com>
2021-09-05 10:37:18 +1200

    common folder added

---
+++ common/src/errors/custom-error.ts
@@ -0,0 +1,12 @@
+//abstract class for all errors
+export abstract class CustomError extends Error {
+    abstract statusCode : number;
+
+    constructor(){
+        super();
+
+        Object.setPrototypeOf(this, CustomError.prototype);
+    }
+
+    abstract serializeErrors(): { message: string; field?: string}[];
+}
